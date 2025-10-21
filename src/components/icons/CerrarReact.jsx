0b5734a3f005da47125c11dc42cbf58ffb59481b
Ruben Cabrerizo <rubencabrerizo17@gmail.com>
2025-10-21 22:54:20 +0200

    feat:addIcons

--- src/components/icons/CerrarReact.jsx
+++ src/components/icons/CerrarReact.jsx
@@ -6,13 +6,13 @@ export default function Cerrar (){
             height="16"
             fill="none"
             viewBox="0 0 16 16"
-            class="text-primaryText"
+            className="text-primaryText"
         >
             <path
                 fill="currentColor"
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M7.116 8l-4.558 4.558.884.884L8 8.884l4.558 4.558.884-.884L8.884 8l4.558-4.558-.884-.884L8 7.116 3.442 2.558l-.884.884L7.116 8z"
-                clip-rule="evenodd"></path>
+                clipRule="evenodd"></path>
         </svg>
 
     )
