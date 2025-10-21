6e19110dffb9f844850eb18dc8405ef745b8d888
Ruben Cabrerizo <rubencabrerizo17@gmail.com>
2025-10-21 23:15:16 +0200

    change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: "https://rubo17.github.io",
-  base: "/portfolio/",
+  base: "/",
   integrations: [tailwind(),react()],
 }); 
